b4bb7e76a2b67bffec2fd9f0e0c1dd21feee1a98
David Abell <david.abell.m@gmail.com>
2022-04-28 19:34:41 +0100

    refactor to class

---
+++ GameBoard.js
@@ -0,0 +1,110 @@
+"use-strict";
+
+class GameBoard {
+  canvas;
+
+  context;
+
+  gridReference = [];
+
+  snakes = {
+    16: -10,
+    46: -21,
+    49: -38,
+    62: -43,
+    64: -4,
+    74: -21,
+    89: -21,
+    92: -4,
+    95: -20,
+    99: -19,
+  };
+
+  ladders = {
+    2: 36,
+    7: 7,
+    8: 23,
+    15: 11,
+    21: 21,
+    28: 56,
+    36: 8,
+    51: 16,
+    71: 20,
+    78: 20,
+    87: 7,
+  };
+
+  constructor(containerEl, size) {
+    this.containerEl = containerEl;
+    this.boardSize = size;
+    this.init();
+  }
+
+  setCanvas() {
+    if (!document.getElementById("game-board")) {
+      const canvas = document.createElement("canvas");
+      canvas.setAttribute("id", "game-board");
+      document.getElementById("game-container").appendChild(canvas);
+    }
+    this.canvas = document.getElementById("game-board");
+    this.canvas.width = this.boardSize;
+    this.canvas.height = this.boardSize;
+    this.context = this.canvas.getContext("2d");
+  }
+
+  drawGridLines() {
+    const { width } = this.canvas;
+    const { height } = this.canvas;
+    const gridCount = width / 10;
+
+    this.context.beginPath();
+    this.context.strokeStyle = "black";
+    this.context.lineWidth = 1;
+
+    // Draw grid lines
+    for (let x = 0; x <= width; x += gridCount) {
+      this.context.moveTo(x, 0);
+      this.context.lineTo(x, height);
+    }
+
+    for (let y = 0; y <= height; y += gridCount) {
+      this.context.moveTo(0, y);
+      this.context.lineTo(width, y);
+    }
+  }
+
+  drawLettersAndSaveCoordinates() {
+    const { width } = this.canvas;
+    const { height } = this.canvas;
+    const gridCount = width / 10;
+
+    // Create grid reference and Draw grid numbers
+    this.context.stroke();
+
+    this.context.font = "30px Arial";
+    this.context.textAlign = "center";
+    this.context.textBaseline = "middle";
+
+    for (let i = 0; i < 100; i += 1) {
+      const isEvenRow = Math.floor(i / 10) % 2 === 0;
+      const yOffset = Math.floor(i / 10) * gridCount;
+      let squareXStart = gridCount / 2 + gridCount * i - yOffset * 10;
+      if (!isEvenRow) {
+        squareXStart = width - (gridCount / 2 + gridCount * i - yOffset * 10);
+      }
+
+      const squareYStart = height - (gridCount / 2 + yOffset);
+
+      this.context.fillText(`${i + 1}`, squareXStart, squareYStart);
+      this.gridReference.push([squareXStart, squareYStart]);
+    }
+  }
+
+  init() {
+    this.setCanvas();
+    this.drawGridLines();
+    this.drawLettersAndSaveCoordinates();
+  }
+}
+
+export default GameBoard;
