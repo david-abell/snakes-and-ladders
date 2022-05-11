612dd03a814fc8fd06d0618c5c1d931b6f0ae14f
David Abell <david.abell.m@gmail.com>
2022-05-11 16:35:48 +0100

    remove commented code

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 "use-strict";
 
 import SnakesAndLadders from "./SnakesAndLadders.js";
-// import { debounce } from "./helpers.js";
 
-// const gridContainer = document.getElementById("grid-container");
 const gameBoards = document.getElementById("board-container");
 const messages = document.querySelector("#messages");
 const game = new SnakesAndLadders(gameBoards, messages, 1000);
 
-const playButton = document.querySelector("#play-turn");
-
-// window.addEventListener(
-//   "resize",
-//   debounce(() => {
-//     game.init;
-//   })
-// );
+const playButton = document.querySelector("#play-button");
 
 playButton.addEventListener("click", () => {
   game.play();
